1c8dbdc8bfcffc72a1187cddbd3ccd78a2825c09
phanhuy <phanhuy.bkhn@gmail.co>
2014-11-24 21:36:15 +0700

    finish signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
@@ -23,5 +24,3 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
-
- 

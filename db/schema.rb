b9aa6f34bd1328b515efdaa99fe0de061fa55f96
phanhuy <phanhuy.bkhn@gmail.co>
2014-12-08 22:24:51 +0700

    Finish log in/log out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122175758) do
+ActiveRecord::Schema.define(version: 20141208145406) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141122175758) do
     t.datetime "updated_at"
     t.string   "password"
     t.string   "password_confirmation"
+    t.string   "remember_digest"
   end
 
 end
